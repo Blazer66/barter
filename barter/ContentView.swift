2e1c2b0067dd6d236e0844b392a13873c83e3209
Chrisite L Richardson <crichardson@catholiccharities.net>
2021-04-25 00:10:11 -0500

    I started all over!

--- barter/ContentView.swift
+++ barter/ContentView.swift
@@ -8,8 +8,9 @@
 import SwiftUI
 
 struct ContentView: View {
+    
     var body: some View {
-        Text("Hello, world!")
+        Home()
             .padding()
     }
 }
@@ -19,3 +20,67 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+struct Home : View{
+    
+    @State var index = 0
+    var body: some View{
+        
+        VStack{
+            
+            Image("logo")
+                .resizable()
+                .aspectRatio(contentMode: /*@START_MENU_TOKEN@*/.fill/*@END_MENU_TOKEN@*/)
+                .frame(width: 70, height: 70)
+                .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+            
+            Spacer()
+            
+            HStack{
+                Button(action: {
+                    withAnimation(.spring()){}
+                    index = 0
+                    
+                }){
+                    VStack{
+                        
+                        Text("Login")
+                            .font(.system(size: 20))
+                            .fontWeight(.bold)
+                            .foregroundColor(index == 0 ? .black : .gray)
+                        
+                        Capsule()
+                            .fill(Color("Color"))
+                            .frame(height: 4)
+                    }
+                }
+                
+                
+                Button(action: {
+                    
+                    withAnimation(.spring()){
+                        
+                        index = 0
+                    }
+                    
+                }) {
+                    
+                    VStack{
+                        
+                        Text("Sign Up")
+                            .font(.system(size: 20))
+                            .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                            .foregroundColor(index == 1 ? .black: .gray)
+                        
+                        Capsule()
+                            .fill(Color("Color"))
+                            .frame(height: 4)
+                    }
+                }
+            }
+        }
+        .padding(.top, 40)
+        
+    }
+}
+
